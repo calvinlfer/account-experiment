3a86e997e7b367af33aa322b34c7d9f1ec331e4f
Calvin L. Fernandes <calvin.l.fer@outlook.com>
2017-10-25 00:21:07 -0400

    Apply formatting to tests Bring in scoverage

--- src/test/scala/AccountServiceSpecification.scala
+++ src/test/scala/AccountServiceSpecification.scala
@@ -1,12 +1,13 @@
 import com.calvin.experiments.domain.AccountService
 import com.calvin.experiments.domain.interpreters.simple.SimpleAccount._
 import com.calvin.experiments.domain.interpreters.simple._
-import org.scalacheck.{Gen, Prop, Properties}
+import org.scalacheck.{ Gen, Prop, Properties }
 import Prop.forAll
 
-class AccountServiceSpecification extends Properties("Account Algebra Verification via SimpleAccount")
-  with AccountService[MoneyBD, SimpleAccount, ErrorOr]
-  with SimpleAccountService {
+class AccountServiceSpecification
+    extends Properties("Account Algebra Verification via SimpleAccount")
+    with AccountService[MoneyBD, SimpleAccount, ErrorOr]
+    with SimpleAccountService {
 
   // Generators will provide different data for each Property test
   val accountGen: Gen[SimpleAccount] = for {
@@ -22,35 +23,31 @@ class AccountServiceSpecification extends Properties("Account Algebra Verificati
       val s1 = credit(amount)(account)
       val updatedAccount = debit(amount)(s1)
       updatedAccount.isRight &&
-        updatedAccount.right.get == account
+      updatedAccount.right.get == account
     }
 
-  property(
-    """A pre-authorization followed by a capture of $X will cause a reduction to the account of $X and must not
-      |affect an existing holding balances""".stripMargin) =
-    forAll(accountGen) { initialAccount =>
-      val amount = 10
-      val updatedAccount = for {
-        s1 <- preAuth(amount)(initialAccount)
-        s2 <- capture(amount)(s1)
-      } yield s2
+  property("""A pre-authorization followed by a capture of $X will cause a reduction to the account of $X and must not
+      |affect an existing holding balances""".stripMargin) = forAll(accountGen) { initialAccount =>
+    val amount = 10
+    val updatedAccount = for {
+      s1 <- preAuth(amount)(initialAccount)
+      s2 <- capture(amount)(s1)
+    } yield s2
 
-      updatedAccount.isRight &&
-        updatedAccount.right.get.balance == initialAccount.balance - amount &&
-        updatedAccount.right.get.balanceHeld == initialAccount.balanceHeld
-    }
+    updatedAccount.isRight &&
+    updatedAccount.right.get.balance == initialAccount.balance - amount &&
+    updatedAccount.right.get.balanceHeld == initialAccount.balanceHeld
+  }
 
-  property(
-    """A purchase (acts like a debit) of $X will reduce the balance in the account by $X without touching
-      |holding balances""".stripMargin) =
-    forAll(accountGen) { initialAccount =>
-      val amount = 10
-      val updatedAccount = purchase(amount)(initialAccount)
+  property("""A purchase (acts like a debit) of $X will reduce the balance in the account by $X without touching
+      |holding balances""".stripMargin) = forAll(accountGen) { initialAccount =>
+    val amount = 10
+    val updatedAccount = purchase(amount)(initialAccount)
 
-      updatedAccount.isRight &&
-        updatedAccount.right.get.balance == initialAccount.balance - amount &&
-        updatedAccount.right.get.balanceHeld == initialAccount.balanceHeld
-    }
+    updatedAccount.isRight &&
+    updatedAccount.right.get.balance == initialAccount.balance - amount &&
+    updatedAccount.right.get.balanceHeld == initialAccount.balanceHeld
+  }
 
   property("A pre-authorization of $X followed by a cancellation of $X will not change the account balances") =
     forAll(accountGen) { initialAccount =>
